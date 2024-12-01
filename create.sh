76f64758d47da0c1e6891103e03f7d8f6b70ebf2
Yannick de Jong <ydejong@ignitioncomputing.com>
2024-12-01 11:48:23 +0100

    Spruced up the create template

--- create.sh
+++ create.sh
@@ -6,12 +6,34 @@ template=$(cat <<EOF
 const std = @import("std");
 const print = std.debug.print;
 
-pub fn main() !void {
-    const input = @embedFile("sample.txt");
-    var it = std.mem.tokenizeAny(u8, input, "\n");
+fn part1(input: []const u8) !void {
+    var it = std.mem.splitScalar(u8, input, '\n');
     while (it.next()) |line| {
         print("{s}\n", .{line});
     }
+}
+
+fn part2(input: []const u8) !void {
+    var it = std.mem.splitScalar(u8, input, '\n');
+    while (it.next()) |line| {
+        print("{s}\n", .{line});
+    }
+}
+
+pub fn main() !void {
+    const input = @embedFile("input.txt");
+    try part1(input);
+    try part2(input);
+}
+
+test "Part 1 - sample" {
+    const input = @embedFile("sample.txt");
+    try part1(input);
+}
+
+test "Part 2 - sample" {
+    const input = @embedFile("sample.txt");
+    try part2(input);
 }
 EOF
 )
